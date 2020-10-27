566fed88ed43c8022a7e8de6948e998ef110dd5b
Darina Bird <darinabird46722@gmail.com>
2020-10-27 14:44:31 +0300

    correct version

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,6 +4,5 @@ from .base_page import BasePage
 class MainPage(BasePage):
     def go_to_login_page(self):
         
-        # неясно почему не импортируется self как browser ??
-        login_link = self.find_element_by_css_selector('#login_link')
+        login_link = self.browser.find_element_by_css_selector('#login_link')
         login_link.click()
