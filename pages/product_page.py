56de1b28f214cb1f63edb96415c3afe22f33d9a9
Darina Bird <darinabird46722@gmail.com>
2020-11-01 14:11:27 +0300

    need help with it

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,11 +1,41 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-    def add_item_to_basket(self):
+    def check_basket(self):
+        self.add_item()
+        self.naming_equality()
+        self.check_price_equality()
 
-        b_button = self.browser.find_element(By.CSS_SELECTOR, "")
+        link = self.browser.find_element(*ProductPageLocators.product_url)
+        link.click()
+        return ProductPage(browser=self.browser, url=self.browser.current_url)
+
+    def add_item(self, browser):
+        '''
+        Описать метод добавления в корзину.
+        '''
+        b_button = browser.find_element(*ProductPageLocators.b_button)
         b_button.click()
 
-    
+
+    def naming_equality(self):
+        '''
+        Сообщение о том, что товар добавлен в корзину.
+        Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили.
+
+        --сейчас стоит, та проверка, которую смогла написать--
+        '''
+        assert self.is_element_present(
+            *ProductPageLocators.item_name), 'Element isn\'t here'
+
+    def check_price_equality(self):
+        '''
+        Сообщение со стоимостью корзины. Стоимость корзины совпадает с ценой товара. 
+
+        --сейчас стоит, та проверка, которую смогла написать--
+        '''
+        assert self.is_element_present(
+            *ProductPageLocators.item_price), 'Element isn\'t here'
