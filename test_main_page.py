83aab2a48ee0bbd7ffcfea71c24a30ad8eb4bee3
Darina Bird <darinabird46722@gmail.com>
2020-11-01 14:11:17 +0300

    just add

--- test_main_page.py
+++ test_main_page.py
@@ -3,12 +3,10 @@ from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
-    page.open()                      # открываем страницу
-    # выполняем метод страницы — переходим на страницу логина
-    page.go_to_login_page
+    page.open()
+    page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
