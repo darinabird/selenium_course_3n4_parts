71bf7e717384d408efef33f8f444762ae6a1cff3
kee-rea1 <6m8o3k7r29>
2020-11-02 15:18:03 +0400

    kek

--- pages/locators.py
+++ pages/locators.py
@@ -10,9 +10,11 @@ class LoginPageLocators():
     login_form = (By.CSS_SELECTOR, "#login_form")
     register_form = (By.CSS_SELECTOR, "#register_form")
 
+
 class ProductPageLocators():
     product_url = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    b_button = (By.ID, '#add_to_basket_form')
+    pre_find = (By.CSS_SELECTOR, "form[id=\"add_to_basket_form\"]")
+    b_button = (By.CSS_SELECTOR, "button[type=\"submit\"]")
     item_name = (By.CSS_SELECTOR, '.alertinner strong')
     item_price = (By.CSS_SELECTOR, 'p.price_color')
     total_price = (By.CLASS_NAME, '.basket-mini strong')
