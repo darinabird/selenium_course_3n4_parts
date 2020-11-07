82db7dbc43348477941754c165780203f2e1879a
Darina Bird <darinabird46722@gmail.com>
2020-11-07 19:44:35 +0300

    removed unneccesary "browser" parameter, replaced it by "self" call

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,20 +4,22 @@ from pages.locators import ProductPageLocators
 
 
 class ProductPage(BasePage, ProductPageLocators):
-    def check_basket(self, browser):
-        self.add_item(browser)
-        self.naming_equality()
-        self.check_price_equality()
+    # --- unnecessary part ---
+    
+    # def check_basket(self, browser):
+    #     self.add_item(browser)
+    #     self.naming_equality()
+    #     self.check_price_equality()
 
-        link = self.browser.find_element(*ProductPageLocators.product_url)
-        link.click()
-        return ProductPage(browser=self.browser, url=self.browser.current_url)
+    #     link = self.browser.find_element(*ProductPageLocators.product_url)
+    #     link.click()
+    #     return ProductPage(browser=self.browser, url=self.browser.current_url)
 
-    def add_item(self, browser):
+    def add_item(self):
         '''
         Описать метод добавления в корзину.
         '''
-        b_button = browser.find_element(*ProductPageLocators.b_button)
+        b_button = self.browser.find_element(*ProductPageLocators.b_button)
         b_button.click()
         # активация скидки
         self.solve_quiz_and_get_code()
--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,15 @@
 from pages.product_page import ProductPage
+from pages.locators import ProductPageLocators
 from pages.base_page import BasePage
-from pages.main_page import MainPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    page = MainPage(browser, link)
+    link = ProductPageLocators.product_url
+    page = ProductPage(browser, link)
     page.open()
+    page.add_item()
+    product_page = ProductPage(browser, browser.current_url)
+    product_page.add_item()
 
 # вызвать методы из product_page.py
 # проверить цену книги с ценой корзины
